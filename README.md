e9bdb431c64aaa93daba9a8db6a0eed65a5bf889
Ryan <ryan.ryanstocke@gmail.com>
2018-12-19 22:41:57 -0700

    htmlroutes built and server

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ You will use Express to handle routing. Make sure you deploy your app to Heroku 
 
 1. Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
 
-2. Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
+<!-- 2. Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`. -->
 
 3. Your `htmlRoutes.js` file should include two routes:
 
---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
