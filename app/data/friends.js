ce7b76defd716b57b7630f03b13cdf787a9cd2aa
ryanstocke <ryanstocke@gmail.com>
2018-12-20 23:37:35 -0700

    in progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
-var friendsArray = [{
+var friendsArray = [
+    {
     "name": "Max",
     "photo": "https://www.instagram.com/p/BrDkHvWnw36/",
     "sex": "male",
@@ -14,7 +15,43 @@ var friendsArray = [{
         4,
         1
     ]
-}];
+},
+{
+    "name": "Blue",
+    "photo": "https://www.instagram.com/p/Borym8zHXwH/",
+    "sex": "male",
+    "scores": [
+        2,
+        2,
+        1,
+        1,
+        5,
+        1,
+        5,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Bill Burr",
+    "photo": "http://www.massmutualcenter.com/assets/img/BillBurr_650x370-35f38ac9ae.jpg",
+    "sex": "male",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}
+
+];
 
 module.exports = friendsArray;
 
